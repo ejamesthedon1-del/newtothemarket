752723de6075ec6e2a6bf7629b6d8cce3fd418fa
ejamesthedon1-del <ejamesthedon1@gmail.com>
2025-10-31 20:10:01 -0500

    Delete app dashboard

--- app/global.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "use-places-autocomplete" {
-  const usePlacesAutocomplete: any;
-  export const getGeocode: any;
-  export const getLatLng: any;
-  export default usePlacesAutocomplete;
-}
